9414accc351673fe8576fdcf526382f4cbc267b7
fuyuanshun <fuyuanshun521@gmail.com>
2022-09-23 18:15:50 +0800

    增加elasticsearch组件. redis组件配置修改

---
+++ cloud-commons/common-redis/src/main/java/com/fys/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.fys.config;
+
+import com.fys.properties.RedisProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @author fys
+ * @date 2022/9/20
+ * @description
+ */
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisConfig {
+
+    @Autowired
+    private RedisProperties redisProperties;
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(redisProperties.getMaxActive());
+        jedisPoolConfig.setMaxIdle(redisProperties.getMaxIdle());
+        jedisPoolConfig.setMinIdle(redisProperties.getMinIdle());
+        jedisPoolConfig.setMaxWaitMillis(redisProperties.getMaxWaitMillis());
+        jedisPoolConfig.setTestOnBorrow(redisProperties.getTestOnBorrow());
+        return jedisPoolConfig;
+    }
+
+    @Bean
+    public JedisPool jedisPool(JedisPoolConfig jedisPoolConfig) {
+
+        if (redisProperties.getPassword() != null && !"".equals(redisProperties.getPassword())) {
+            return new JedisPool(jedisPoolConfig, redisProperties.getHost(),
+                    Integer.parseInt(redisProperties.getPort()), redisProperties.getTimeout(), redisProperties.getPassword());
+        }
+        return new JedisPool(jedisPoolConfig, redisProperties.getHost(), Integer.parseInt(redisProperties.getPort()), redisProperties.getTimeout());
+    }
+}
---
+++ cloud-commons/common-redis/src/main/java/com/fys/properties/RedisProperties.java
@@ -0,0 +1,102 @@
+package com.fys.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author fys
+ * @date 2022/9/20
+ * @description
+ */
+@ConfigurationProperties(prefix = "redis")
+public class RedisProperties {
+
+    private Integer maxActive;
+
+    private Integer maxIdle;
+
+    private Integer minIdle;
+
+    private Long maxWaitMillis;
+
+    private Boolean testOnBorrow;
+
+    private String host;
+
+    private String port;
+
+    private String password;
+
+    private Integer timeout;
+
+    public Integer getMaxActive() {
+        return maxActive;
+    }
+
+    public void setMaxActive(Integer maxActive) {
+        this.maxActive = maxActive;
+    }
+
+    public Integer getMaxIdle() {
+        return maxIdle;
+    }
+
+    public void setMaxIdle(Integer maxIdle) {
+        this.maxIdle = maxIdle;
+    }
+
+    public Integer getMinIdle() {
+        return minIdle;
+    }
+
+    public void setMinIdle(Integer minIdle) {
+        this.minIdle = minIdle;
+    }
+
+    public Long getMaxWaitMillis() {
+        return maxWaitMillis;
+    }
+
+    public void setMaxWaitMillis(Long maxWaitMillis) {
+        this.maxWaitMillis = maxWaitMillis;
+    }
+
+    public Boolean getTestOnBorrow() {
+        return testOnBorrow;
+    }
+
+    public void setTestOnBorrow(Boolean testOnBorrow) {
+        this.testOnBorrow = testOnBorrow;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(Integer timeout) {
+        this.timeout = timeout;
+    }
+}
